35f1d65fc9d980507cdbac0b43a4b884ede3925f
tejulade <ladehtejaswini@gmail.com>
2025-02-06 13:09:49 +0530

    jenkins succes

---
+++ jenkins/src/test/java/demo/JenkinsTest.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	@Test
+	public void sample() {
+		
+	Reporter.log("succssfully",true);
+		
+	}
+
+}
