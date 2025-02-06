6336af36d5e14824344b30a72da40c3d72eb1427
tejulade <ladehtejaswini@gmail.com>
2025-02-06 13:56:58 +0530

    PollScmTest class added

---
+++ jenkins/src/test/java/demo/PollScmTest.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollScmTest {
+	@Test
+	public void PollScm() {
+		Reporter.log("PollScmTest class added");
+	}
+
+}
